1f93fcb84b2a0427f21a2a95281ede5afc1cae91
thenaz <anaz7963@740s-pl-126.local>
2016-09-14 08:32:42 -0600

    Added counts for each body part of monster
    9/14

---
+++ src/monster/model/MarshmallowMonster.java
@@ -0,0 +1,28 @@
+package monster.model;
+
+public class MarshmallowMonster
+{
+
+	private String name;
+	private double hairCount;
+	private int eyeCount;
+	private boolean hasBellyButton;
+	private int armCount;
+	private int noseCount;
+	private int jetpackCount;
+	
+	public MarshmallowMonster()
+	{
+		this.name = "no name here";
+		this.hairCount = -23423.131235;
+	    this.eyeCount = -6545;
+	    this.hasBellyButton = false;
+	    this.armCount = -64564;
+	    this.noseCount = -7765;
+	}
+	
+	public MarshmallowMonster(String name, double antennaCount)
+	{
+	this.name = name;
+	}
+}
