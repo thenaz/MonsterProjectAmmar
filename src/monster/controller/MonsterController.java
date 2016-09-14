b5fd8ac6db5aaa8df35e49277115acbc06e3a66c
thenaz <anaz7963@740s-pl-126.local>
2016-09-14 09:49:37 -0600

    Added print statements about my monster
    9/14

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -14,7 +14,12 @@ public class MonsterController
 	
 	public void start()
 	{
-		System.out.println("Here is my monster: " + firstMonster); 
+		System.out.println("Here is my monster: " + firstMonster);
+		System.out.println("My monster has this many eyes: " + firstMonster.getEyeCount());
+		if(firstMonster.getEyeCount() > 3)
+		{
+			System.out.println("Thats a lot of eyes!!!!");
+		}
 	}
 	
 }
